c77e962fb87b43a428347e011ed33e092d51eec3
Your Actual Name <Your Actual Email>
2018-11-22 13:06:31 +0900

    終了期限ありのタスク作成テスト、および、期限順の並び替えテストを作成

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature  "タスク管理機能", type: :feature do
+  let(:now) { Date.current }
   background do
     FactoryBot.create(:task)
-    FactoryBot.create(:task, title: "美容室", content: "16時から")
-    FactoryBot.create(:task, title: "歯医者", content: "18時から")
-    FactoryBot.create(:task, title: "飲み会予約", content: "海浜幕張のちばチャン19時〜")
-    FactoryBot.create(:task, title: "新幹線の予約", content: "東京駅10時発のやつ")
+    FactoryBot.create(:task, title: "美容室", content: "16時から", expired_at: "#{now}")
+    FactoryBot.create(:task, title: "歯医者", content: "18時から", expired_at: "#{now}")
+    FactoryBot.create(:task, title: "飲み会予約", content: "海浜幕張のちばチャン19時〜", expired_at: "#{now.since(1.week)}")
+    FactoryBot.create(:task, title: "新幹線の予約", content: "東京駅10時発のやつ", expired_at: "#{now}")
   end
+
   scenario "タスク一覧のテスト" do
     visit root_path
 
@@ -20,29 +22,45 @@ RSpec.feature  "タスク管理機能", type: :feature do
 
     fill_in 'タスク名', with: "DIC"
     fill_in '内容', with: "オリアプを完成させる"
+    select '2018', from: 'task_expired_at_1i'
+    select '12', from: 'task_expired_at_2i'
+    select '25', from: 'task_expired_at_3i'
 
     click_on "登録する"
     
     expect(page).to have_content "DIC"
     expect(page).to have_content "オリアプを完成させる"
+    expect(page).to have_content "2018-12-25"
   end
 
-  let(:task) { Task.create!(title: "もくもく会", content: "15時〜五反田") }
+  let(:task) { Task.create!(title: "もくもく会", content: "15時〜五反田", expired_at: "#{now}") }
   scenario "タスク詳細のテスト" do
     visit task_path(task.id)
-
+    
     expect(page).to have_content "もくもく会"
     expect(page).to have_content "15時〜五反田"
+    expect(page).to have_content "#{now}"
   end
+  feature "タスクの並び順" do
+    let(:task) { all('tr td') }
+    scenario "タスク一覧が作成日時順に並んでいるかのテスト" do
+      visit root_path
 
-  scenario "タスク一覧が作成日時順に並んでいるかのテスト" do
-    visit root_path
-    
-    task = all('tr td')
-    expect(task[0]).not_to have_text 'Amazon定期便'
-    expect(task[6]).to have_text '海浜幕張のちばチャン19時〜'
+      expect(task[0]).not_to have_text 'Amazon定期便'
+      expect(task[7]).to have_text '海浜幕張のちばチャン19時〜'
 
-    task[12].click_on '詳細'
-    expect(page).to have_selector 'h3', text: '18時から'
+      task[15].click_on '詳細'
+      expect(page).to have_selector 'h3', text: '18時から'
+    end
+
+    scenario "タスクが終了期限順に並び変わるかのテスト" do
+      visit root_path
+
+      click_link '終了期限でならべかえる'
+      expect(task[0]).to have_text 'Amazon定期便'
+
+      task[9].click_on '詳細'
+      expect(page).to have_selector 'h3', text: '飲み会予約'
+    end
   end
 end
