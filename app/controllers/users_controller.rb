535fc042e48ae0b48ee4201daacdeee301863da2
Your Actual Name <Your Actual Email>
2018-11-29 00:26:01 +0900

    userのCRUD機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(show edit udpate destroy)
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録しました！"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def index
+    @users = User.page(params[:page]).per(20)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update
+      flash[:success] = "編集しました！"
+      redirect_to @user
+    else
+      :edit
+    end
+  end
+
+  def destroy 
+    @user.destroy
+    flash[:info] = "削除しました"
+    redirect_to users_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
